90e9bcc9503a457d2d9bb9da94b046c7fe3f1b8a
Davide Pulci <davide.pulci@gmail.com>
2022-05-04 15:18:27 +0200

    inizio

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google FAQ</title>
+</head>
+<body>
+    <!--
+        Riscrivere questa pagina del sito google
+        https://policies.google.com/faq.
+        Ci sono diverse domande con relative risposte.
+        Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP.
+        Mi raccomando di gestire per prima cosa la visualizzazione dei dati, anche in maniera grezza,
+        e solo alla fine rendere più accattivante la pagina. 
+    -->
+    
+</body>
+</html>
